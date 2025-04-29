cbfe44207c6a4be7109ef32ef3100381aa964f06
NEONIX <timuraridi@yandex.ru>
2025-04-29 22:51:24 +0300

    Добавил изображения

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,13 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
+// клонируем содержимое тега template
+const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+// наполняем содержимым
+cardElement.querySelector('.user__avatar').src = 'tinyurl.com/v4pfzwy';
+
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
